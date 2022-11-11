ed689a5a360d3bedcecf7c89a7b5bf8ee10b6ede
muchris <mucyochristian2@gmail.com>
2022-11-11 17:01:16 +0200

    fix link

--- pages/index.tsx
+++ pages/index.tsx
@@ -500,7 +500,7 @@ const Home: NextPage = () => {
                 <div className="flex items-center gap-6 mt-auto">
                   <Link
                     type="button"
-                    href={"/https://expense-app-tracker.vercel.app"}
+                    href={"https://expense-app-tracker.vercel.app"}
                   >
                     <a className="transition font-semibold rounded-md text-sm justify-center inline-flex items-center hover:duration-150 px-4 py-2.5 border border-secondary-1 bg-secondary-1">
                       Demo
